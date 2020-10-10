40802395e752cde102df064c48218e16d90889ba
酷酷哒 <32319187+ymdzha@users.noreply.github.com>
2020-10-10 16:18:00 +0800

    Update string-type.js

--- utils/string-type.js
+++ utils/string-type.js
@@ -4,3 +4,10 @@ export function stringDeleteLast (str) {
   const s1 = str.substring(0, str.length - 6)
   return s1
 }
+
+// 字母大小写取反
+export function NegationStr (str) {
+  return str = str.replace(/[a-zA-Z]/g, content => {
+    return content.toUpperCase() ===  content ?  content.toLowerCase() : content.toUpperCase()
+  })
+}
